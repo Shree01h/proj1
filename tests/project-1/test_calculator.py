50944adf531c3a26fd3de5a51e99be3cd79832fc
Shreelakshmi Shreedhar Hegade <154456034+Shree01h@users.noreply.github.com>
2026-03-04 11:24:57 +0530

    Refactor test cases in test_calculator.py

--- tests/project-1/test_calculator.py
+++ tests/project-1/test_calculator.py
@@ -3,16 +3,17 @@ from proj1.calculator import add
 
 def test_add_positive():
     assert add(2, 3) == 5
-
+# test1
 
 def test_add_negative():
     assert add(-1, -1) == -2
   # hi
 def test_add_positive():
     assert add(1, 3) == 4
-
+# hello
 
 def test_add_negative():
     assert add(-1, -2) == -3
+
 
 
